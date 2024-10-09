7ba14b54e11aa9dd445bf43308fead4a4eed3390
jpsi-0895 <jaipal.saini.0895@gmail.com>
2024-10-09 16:33:18 +0530

    Access specifier

---
+++ java/access_specifier/Access.java
@@ -0,0 +1,18 @@
+
+package java.access_specifier;
+
+/**
+ *  Parent
+ */
+class  Parent {
+
+    void method1(){
+        System.out.println("method 1");
+    }
+    
+}
+public class Access extends Parent{
+    public static void main(String[] args) {
+        
+    }
+}
